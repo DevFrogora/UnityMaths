8ed8dc0546d1863eef750a676e01d0cb11014074
neverDieAgain <rr4428310@gmail.com>
2022-03-08 23:51:45 +0530

    Maths :Chidre Tech 63 distance and angle

---
+++ Assets/6.ChildreTech/63/Editor/ChildreTech63Inspector.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+// To delink the editor script comment the custom Editor header
+[CustomEditor(typeof(ChildreTech63))]
+public class ChildreTech63Inspector : Editor
+{
+    ChildreTech63 cube;
+    void OnEnable()
+    {
+        cube = (ChildreTech63)(target);
+
+    }
+    int currentIndex;
+    int degree;
+    //float  shiftGraph;
+    public override void OnInspectorGUI()
+    {
+        if (GUILayout.Button("calculate distance"))
+        {
+            Debug.Log("distance :  "+GetLength(0,0,cube.transform.position.z, cube.transform.position.y));
+            Debug.Log("Angle  :  "+GetAngle(0, 0, cube.transform.position.z, cube.transform.position.y));
+        }
+    }
+
+    private void OnSceneGUI()
+    {
+
+            Handles.DrawLine(new Vector3(0,0 ,0) , new Vector3(0, cube.transform.position.y, cube.transform.position.z));  // for circular motion
+    }
+
+
+    float GetLength(float x1, float y1 , float x2, float y2)
+    {
+        float x = x2 - x1;
+        float y = y2 - y1;
+
+        float h = Mathf.Sqrt((x * x) + (y * y));
+
+
+        return Mathf.Abs(h);
+    }
+
+    float GetAngle(float x1, float y1, float x2, float y2)
+    {
+        float x = x2 - x1;
+        float y = y2 - y1;
+
+        float angle = Mathf.Atan2(y,x);
+
+
+        return angle * Mathf.Rad2Deg;
+    }
+}
